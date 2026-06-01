79f98dbec5752e0bbd78713fa2637c2cdcd45d8c
bdew <blackdew@gmail.com>
2026-06-01 22:22:13 +0300

    use systemd units for initial partition expansion and store setup as boot.postBootCommands doesn't work right with systemd stage-1

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,39 +1,81 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }:
 {
-  config = {
-    boot.postBootCommands =
-      let
-        nixPathRegistrationFile = "/nix-path-registration";
-      in
-      ''
-        # On the first boot do some maintenance tasks
-        if [ -f ${nixPathRegistrationFile} ]; then
-          set -euo pipefail
-          set -x
 
+  config =
+    let
+      nixPathRegistrationFile = "/nix-path-registration";
+    in
+    {
+
+      # Stolen from nixos/modules/installer/sd-card/sd-image.nix
+
+      systemd.services.expand-root-partition = {
+        description = "Grow the root partition and filesystem to fill the SD card";
+        unitConfig = {
+          DefaultDependencies = false;
+          ConditionPathExists = nixPathRegistrationFile;
+        };
+        wantedBy = [ "sysinit.target" ];
+        before = [
+          "sysinit.target"
+          "shutdown.target"
+          "register-nix-paths.service"
+        ];
+        after = [ "local-fs.target" ];
+        conflicts = [ "shutdown.target" ];
+        restartIfChanged = false;
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+        };
+        script = ''
           # Figure out device names for the boot device and root filesystem.
-          rootPart=$(${pkgs.util-linux}/bin/findmnt -n -o SOURCE /)
-          bootDevice=$(lsblk -npo PKNAME $rootPart)
+          rootPart=$(${lib.getExe' pkgs.util-linux "findmnt"} -n -o SOURCE /)
+          bootDevice=$(${lib.getExe' pkgs.util-linux "lsblk"} -npo PKNAME $rootPart)
+          partNum=$(${lib.getExe' pkgs.util-linux "lsblk"} -npo MAJ:MIN $rootPart | ${lib.getExe pkgs.gawk} -F: '{print $2}')
 
           # Resize the root partition and the filesystem to fit the disk
-          echo ",+," | sfdisk -N1 --no-reread $bootDevice
-          ${pkgs.parted}/bin/partprobe
-          ${pkgs.e2fsprogs}/bin/resize2fs $rootPart
+          echo ",+," | ${lib.getExe' pkgs.util-linux "sfdisk"} -N$partNum --no-reread $bootDevice
+          ${lib.getExe' pkgs.parted "partprobe"}
+          ${lib.getExe' pkgs.e2fsprogs "resize2fs"} $rootPart
+        '';
+      };
 
-          # Register the contents of the initial Nix store
-          ${config.nix.package.out}/bin/nix-store --load-db < ${nixPathRegistrationFile}
+      systemd.services.register-nix-paths = {
+        description = "Register Nix Store Paths";
+        unitConfig = {
+          DefaultDependencies = false;
+          ConditionPathExists = nixPathRegistrationFile;
+        };
+        wantedBy = [ "sysinit.target" ];
+        before = [
+          "sysinit.target"
+          "shutdown.target"
+          "nix-daemon.socket"
+          "nix-daemon.service"
+        ];
+        after = [ "local-fs.target" ];
+        conflicts = [ "shutdown.target" ];
+        restartIfChanged = false;
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+        };
+        script = ''
+          ${lib.getExe' config.nix.package.out "nix-store"} --load-db < ${nixPathRegistrationFile}
 
           # nixos-rebuild also requires a "system" profile and an /etc/NIXOS tag.
           touch /etc/NIXOS
-          ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
+          ${lib.getExe' config.nix.package.out "nix-env"} -p /nix/var/nix/profiles/system --set /run/current-system
 
           # Prevents this from running on later boots.
           rm -f ${nixPathRegistrationFile}
-        fi
-      '';
-  };
+        '';
+      };
+    };
 }
